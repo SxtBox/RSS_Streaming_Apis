fb88e06fa1b472253fdb7ef93f69add24160b65b
Albdroid <sxt@usa.com>
2020-08-29 08:39:21 +0200

    Add files via upload

---
+++ RSS_Merger_AIO_Demo/Main_API_Demo_CL.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ ┌─────────────────────────────────────────────────────────────┐
+ |  For More Modules Or Updates Stay Connected to Kodi dot AL  |
+ └─────────────────────────────────────────────────────────────┘
+ ┌───────────┬─────────────────────────────────────────────────┐
+ │ Product   │ RSS Builder AIO                                 │
+ │ Version   │ v1.4 RC4 Beta Multiple                          │
+ │ Provider  │ https://paidcodes.albdroid.al                   │
+ │ Support   │ JSON/Smart TV/Kodi                              │
+ │ Sources   │ Multiple RSS or XML Data                        │
+ │ Licence   │ MIT                                             │
+ │ Author    │ Olsion Bakiaj                                   │
+ │ Email     │ TRC4@USA.COM                                    │
+ │ Author    │ Endrit Pano                                     │
+ │ Email     │ INFO@ALBDROID.AL                                │
+ │ Website   │ https://kodi.al                                 │
+ │ Facebook  │ /albdroid.official/                             │
+ │ Github    │ github.com/SxtBox/                              │
+ │ Created   │ 29 August 2020                                  │
+ │ Modified  │ 00:00:0000                                      │
+ └─────────────────────────────────────────────────────────────┘
+*/
+
+// HOSTED VERSION https://paidcodes.albdroid.al/RSS_Builder_Apis/JSON_Enclosure/
+
+/*
+ NOTE FOR ALL
+ ALL SERVERS IPS ARE FILTERED THESE ARE ONLY DEMOS AND STREAMING IS NOT ALLOWED FOR XTREAM CODES PANELS OR ANY ANDROID APPS FOR YOUR CLIENTS,
+ IF YOU ARE INTERESTED TO STREAMING YOU CAN BUY THEM AND PUT THEM ON YOUR SERVER!!!
+*/
+
+/*
+ Generator @ Kodi dot AL Dev Tools
+ Code For PHP 5/7
+*/
+
+$API_HOST = "https://paidcodes.albdroid.al/RSS_Builder_Apis/RSS_Merger_AIO/";
+$API_PATH =  "";
+$PHP_FILE =  "index.php";
+
+$API_CALL = $API_HOST. $API_PATH . $PHP_FILE;
+header("Location: $API_CALL");
+?>
